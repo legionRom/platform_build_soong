6eebdf80a07b8a219cfaa12c52af3fc05d5878b1
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with xtended board flags
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

--- scripts/microfactory.bash
+++ scripts/microfactory.bash
@@ -59,7 +59,8 @@ function soong_build_go
     BUILDDIR=$(getoutdir) \
       SRCDIR=${TOP} \
       BLUEPRINTDIR=${TOP}/build/blueprint \
-      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong" \
+      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong
+                  -pkg-path lean/soong=${TOP}/vendor/lean/build/soong" \
       build_go $@
 }
 

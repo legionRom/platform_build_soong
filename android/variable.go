taranhora/build_soong

soong: extend with xtended board flags
Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"xtended/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -114,6 +116,9 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+
+		// include Carbon variables
+		*android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -231,6 +236,9 @@ type productVariables struct {
 	PgoAdditionalProfileDirs []string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
+
+	// include Carbon variables
+	*android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {

scripts/microfactory.bash
@@ -59,7 +59,8 @@ function soong_build_go
     BUILDDIR=$(getoutdir) \
       SRCDIR=${TOP} \
       BLUEPRINTDIR=${TOP}/build/blueprint \
-      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong" \
+      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong
+                  -pkg-path xtended/soong=${TOP}/vendor/xtended/build/soong" \
       build_go $@
 }
 

legionRom/platform_build_soong

android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"lean/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -114,6 +116,9 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+
+		// include Carbon variables
+		*android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -231,6 +236,9 @@ type productVariables struct {
 	PgoAdditionalProfileDirs []string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
+
+	// include Carbon variables
+	*android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {

scripts/microfactory.bash
@@ -59,7 +59,8 @@ function soong_build_go
     BUILDDIR=$(getoutdir) \
       SRCDIR=${TOP} \
       BLUEPRINTDIR=${TOP}/build/blueprint \
-      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong" \
+      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong
+                  -pkg-path lean/soong=${TOP}/vendor/lean/build/soong" \
       build_go $@
 }
 