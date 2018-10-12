taranhora/build_soong

We need Xtended Variable

android/variable.go
@@ -117,8 +117,8 @@ type variableProperties struct {
 			Srcs         []string
 		}
 
-		// include Carbon variables
-		*android.Product_variables
+		// include Xtended variables
+		Xtended android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -237,8 +237,8 @@ type productVariables struct {
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
 
-	// include Carbon variables
-	*android.ProductVariables
+	// include Xtended variables
+	Xtended android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {

legionRom/platform_build_soong

android/variable.go
@@ -117,8 +117,8 @@ type variableProperties struct {
 			Srcs         []string
 		}
 
-		// include Carbon variables
-		*android.Product_variables
+		// include Lean variables
+		Lean android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -237,8 +237,8 @@ type productVariables struct {
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
 
-	// include Carbon variables
-	*android.ProductVariables
+	// include Lean variables
+	Lean android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {